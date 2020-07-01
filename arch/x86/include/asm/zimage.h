3513547b9c941de36b3a914cc066d219c8e582d3
Ram Muthiah <rammuthiah@google.com>
2020-06-30 17:12:20 -0700

    ANDROID: Support bigger bzImages
    They can be bigger than 15mb compressed.
    
    Change-Id: I675eda32abb9d64fcd5e8ac89a5ede76c7f609a8
    Signed-off-by: Ram Muthiah <rammuthiah@google.com>

--- arch/x86/include/asm/zimage.h
+++ arch/x86/include/asm/zimage.h
@@ -22,7 +22,7 @@
 #define COMMAND_LINE_MAGIC  0xA33F
 
 /* limits */
-#define BZIMAGE_MAX_SIZE   15*1024*1024     /* 15MB */
+#define BZIMAGE_MAX_SIZE   16*1024*1024     /* 15MB */
 #define ZIMAGE_MAX_SIZE    512*1024         /* 512k */
 #define SETUP_MAX_SIZE     32768
 
