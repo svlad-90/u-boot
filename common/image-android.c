771f651b6bcf84aa7a6b7106a02389e0406b1976
Alistair Delva <adelva@google.com>
2021-02-12 15:37:48 -0800

    ANDROID: Increase ramdisk alignment to 64MB
    This works around a problem with the arm64 Image image_size header.
    
    Bug: 180142608
    Change-Id: I8929690339bb5a18b338501b36cbb7016389b0f5
    Signed-off-by: Alistair Delva <adelva@google.com>

--- common/image-android.c
+++ common/image-android.c
@@ -166,8 +166,9 @@ static void _populate_boot_info(const struct boot_img_hdr_v3* boot_hdr,
 		sizeof(vboot_hdr->cmdline));
 
 	boot_info->kernel_addr = (ulong)load_addr;
-	boot_info->vendor_ramdisk_addr = boot_info->kernel_addr
-		+ ALIGN(boot_info->kernel_size, SZ_2M);
+	/* The "kernel_addr" is already aligned to 2MB */
+	boot_info->vendor_ramdisk_addr = boot_info->kernel_addr +
+			ALIGN(boot_info->kernel_size, SZ_64M);
 	boot_info->boot_ramdisk_addr = boot_info->vendor_ramdisk_addr
 		+ boot_info->vendor_ramdisk_size;
 }
