1eff171e613ee67dca71dbe97be7282e2db17011
Will Deacon <willdeacon@google.com>
2022-01-20 13:29:25 +0000

    ANDROID: virtio: Use bounce buffers when VIRTIO_F_IOMMU_PLATFORM is set
    Devices advertising the VIRTIO_F_IOMMU_PLATFORM feature require
    platform-specific handling to configure their DMA transactions.
    
    When handling virtio descriptors for such a device, use bounce
    buffers to ensure that the underlying buffers are always aligned
    to and padded to PAGE_SIZE in preparation for platform specific
    handling at page granularity.
    
    Bug: 209797374
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I3f80ea2674e9efa5f4ad15063b9839b867aff381

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -28,18 +28,65 @@ static void virtio_free_pages(struct udevice *vdev, void *ptr, u32 npages)
 	free(ptr);
 }
 
+static int __bb_force_page_align(struct bounce_buffer *state)
+{
+	const ulong align_mask = PAGE_SIZE - 1;
+
+	if ((ulong)state->user_buffer & align_mask)
+		return 0;
+
+	if (state->len != state->len_aligned)
+		return 0;
+
+	return 1;
+}
+
 static void virtqueue_attach_desc(struct virtqueue *vq, unsigned int idx,
 				  struct virtio_sg *sg, u16 flags)
 {
 	struct vring_desc *desc = &vq->vring.desc[idx];
+	void *addr;
+
+	if (IS_ENABLED(CONFIG_BOUNCE_BUFFER) && vq->vring.bouncebufs) {
+		struct bounce_buffer *bb = &vq->vring.bouncebufs[idx];
+		unsigned int bbflags;
+		int ret;
+
+		if (flags & VRING_DESC_F_WRITE)
+			bbflags = GEN_BB_WRITE;
+		else
+			bbflags = GEN_BB_READ;
+
+		ret = bounce_buffer_start_extalign(bb, sg->addr, sg->length,
+						   bbflags, PAGE_SIZE,
+						   __bb_force_page_align);
+		if (ret) {
+			debug("%s: failed to allocate bounce buffer (length 0x%zx)\n",
+			      vq->vdev->name, sg->length);
+			BUG();
+		}
+
+		addr = bb->bounce_buffer;
+	} else {
+		addr = sg->addr;
+	}
 
 	desc->flags	= cpu_to_virtio16(vq->vdev, flags);
-	desc->addr	= cpu_to_virtio64(vq->vdev, (u64)(uintptr_t)sg->addr);
+	desc->addr	= cpu_to_virtio64(vq->vdev, (u64)(uintptr_t)addr);
 	desc->len	= cpu_to_virtio32(vq->vdev, sg->length);
 }
 
 static void virtqueue_detach_desc(struct virtqueue *vq, unsigned int idx)
 {
+	struct vring_desc *desc = &vq->vring.desc[idx];
+	struct bounce_buffer *bb;
+
+	if (!IS_ENABLED(CONFIG_BOUNCE_BUFFER) || !vq->vring.bouncebufs)
+		return;
+
+	bb = &vq->vring.bouncebufs[idx];
+	bounce_buffer_stop(bb);
+	desc->addr = cpu_to_virtio64(vq->vdev, (u64)(uintptr_t)bb->user_buffer);
 }
 
 int virtqueue_add(struct virtqueue *vq, struct virtio_sg *sgs[],
