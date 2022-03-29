878be5822b6616922a6c5544f72ec14f3f41bafe
Andrew Scull <ascull@google.com>
2022-03-29 16:58:51 +0000

    ANDROID: pci: Add config for Enhanced Allocation
    Add a config to control whether Enhanced Allocation is supported by the
    driver.
    
    Signed-off-by: Andrew Scull <ascull@google.com>
    Change-Id: I1a164a56d63484ac6f613748e166a128bc8e04b7

--- drivers/pci/pci-uclass.c
+++ drivers/pci/pci-uclass.c
@@ -643,7 +643,11 @@ int dm_pci_hose_probe_bus(struct udevice *bus)
 		return log_msg_ret("probe", -EINVAL);
 	}
 
-	ea_pos = dm_pci_find_capability(bus, PCI_CAP_ID_EA);
+	if (IS_ENABLED(CONFIG_PCI_ENHANCED_ALLOCATION))
+		ea_pos = dm_pci_find_capability(bus, PCI_CAP_ID_EA);
+	else
+		ea_pos = 0;
+
 	if (ea_pos) {
 		dm_pci_read_config8(bus, ea_pos + sizeof(u32) + sizeof(u8),
 				    &reg);
@@ -1604,7 +1608,11 @@ void *dm_pci_map_bar(struct udevice *dev, int bar, int flags)
 	 * Incase of virtual functions, pdata will help read VF BEI
 	 * and EA entry size.
 	 */
-	ea_off = dm_pci_find_capability(udev, PCI_CAP_ID_EA);
+	if (IS_ENABLED(CONFIG_PCI_ENHANCED_ALLOCATION))
+		ea_off = dm_pci_find_capability(udev, PCI_CAP_ID_EA);
+	else
+		ea_off = 0;
+
 	if (ea_off)
 		return dm_pci_map_ea_bar(udev, bar, flags, ea_off, pdata);
 
